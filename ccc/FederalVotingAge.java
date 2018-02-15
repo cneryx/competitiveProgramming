ec717c91d1dff18f28185ee037636d0d5a518a5a
Chris Xie <36287077+cneryx@users.noreply.github.com>
2018-02-14 21:42:22 -0500

    start

---
+++ ccc/FederalVotingAge.java
@@ -0,0 +1,32 @@
+package ccc07;
+
+import java.util.Scanner;
+
+public class FederalVotingAge {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		for (int i = 0; i < n; i++) {
+			int y = scanner.nextInt();
+			int m = scanner.nextInt();
+			int d = scanner.nextInt();
+			if (y < 1989) {
+				System.out.println("Yes");
+			} else if (y > 1989) {
+				System.out.println("No");
+			} else if (m > 2) {
+				System.out.println("No");
+			} else if (m < 2) {
+				System.out.println("Yes");
+			} else if (d > 27) {
+				System.out.println("No");
+			} else if (d <= 27) {
+				System.out.println("Yes");
+			}
+		}
+
+	}
+
+}
