f2d01efc04e4afb2c336e9e6a1633547d0386572
Chris Xie <36287077+cneryx@users.noreply.github.com>
2018-02-14 21:39:23 -0500

    start

---
+++ ccc/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		int c = scanner.nextInt();
+		int d = scanner.nextInt();
+		int u = scanner.nextInt();
+		
+		int x = Math.abs(c - a);
+		int y = Math.abs(d - b);
+		u -= (x + y);
+		if(u % 2 == 0 && u >= 0) {
+			System.out.println("Y");
+		}
+		else {
+			System.out.println("N");
+		}
+		
+	}
+
+}
